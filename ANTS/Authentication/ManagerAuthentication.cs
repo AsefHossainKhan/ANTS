f7e3c4d3ed077edc591be6b5eb9e386ef66cd1fa
shawonsys <tawhiduzzamans@gmail.com>
2021-07-06 23:26:32 +0600

    Manager Added

--- ANTS/Authentication/ManagerAuthentication.cs
+++ ANTS/Authentication/ManagerAuthentication.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace ANTS.Authentication
 {
-    public class SellerAuthentication : ActionFilterAttribute
+    public class ManagerAuthentication : ActionFilterAttribute
     {
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
@@ -15,7 +15,7 @@ namespace ANTS.Authentication
                     filterContext.Controller.TempData["ErrorMessage"] = "Please Login In!";
                     filterContext.Result = new RedirectResult("/Login/Index");
                 }
-                else if (!filterContext.HttpContext.Session["user_type"].Equals("Seller"))
+                else if (!filterContext.HttpContext.Session["user_type"].Equals("Manager"))
                 {
                     filterContext.Controller.TempData["ErrorMessage"] = "Invalid Request!";
                     filterContext.Result = new RedirectResult("/Login/Index");
